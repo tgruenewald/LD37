8653c92259d1e0a59329795f0e7d76d6a3919656
Terry Gruenewald <tgruenewald@users.noreply.github.com>
2016-12-10 12:17:21 -0700

    Revert "Cleanup"

--- Assets/scripts/gameManager.cs
+++ Assets/scripts/gameManager.cs
@@ -8,6 +8,7 @@ public class gameManager : MonoBehaviour {
 	DialogueParser parser;
 	public roomScript roomScript;
 	public storyManager storyManager;
+	public mapTracker mapTracker;
 	public animateText animateText;
 
 
@@ -77,11 +78,27 @@ public class gameManager : MonoBehaviour {
 		inStory = false;
 		expRound = false;
 		weekText.text = "Week: " + week;
+		mapTracker.hidePlanets ();
 		updateStats ();
 		roomScript.leaveRoom ();
 
 	}
-		
+
+/*	void nextScreen(){
+		if (expRound) {
+			roomScript.gotoPlanet (mapTracker.location);
+		}
+		else if (gainingSkill)
+		{
+			gainSkill ();
+		}
+
+		else{
+			//AdvanceWeek();
+			roomScript.leaveRoom ();
+		}
+
+	}*/
 
 	public void gainSkill(){
 		if (expRound) {
