7094f71f7d394484d7ef14003dbe5a5f588aceb8
tgruenewald <terrygruenewald@gmail.com>
2016-12-11 15:26:06 -0700

    added Fa

---
+++ Assets/scripts/load_game.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class load_game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void load_level() {
+		Application.LoadLevel("hospital_bed");
+		
+	}
+
+	public void load_credits() {
+		Application.LoadLevel("credits");
+
+	}
+
+	public void load_title() {
+		Application.LoadLevel("title");
+
+	}
+}
