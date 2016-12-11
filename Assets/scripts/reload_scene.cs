7a183f5eac31816e77c233ef9aef9abffe39a016
tgruenewald <terrygruenewald@gmail.com>
2016-12-11 12:47:18 -0700

    added reload the scene button

---
+++ Assets/scripts/reload_scene.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class reload_scene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void reload() {
+		Debug.Log("-----reloading scene");
+		Application.LoadLevel(Application.loadedLevel);
+	}
+}
